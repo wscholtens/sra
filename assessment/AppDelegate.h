b6cc10717a60454ff4d1018151ae7ab252eb6baa
Wouter Scholtens <wouter.scholtens@atos.net>
2014-01-07 22:11:55 +0100

    Initial Commit

---
+++ assessment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  assessment
+//
+//  Created by Wouter Scholtens on 1/7/14.
+//  Copyright (c) 2014 Wouter Scholtens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ assessment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  assessment
+//
+//  Created by Wouter Scholtens on 1/7/14.
+//  Copyright (c) 2014 Wouter Scholtens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
